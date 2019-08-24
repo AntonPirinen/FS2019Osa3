b117c889a381a4ea0871036882e9816c32950f5d
Anton Pirinen <anton.pirinen@helsinki.fi>
2019-08-24 15:05:05 +0300

    frontend

---
+++ build/precache-manifest.8e511587c030a03ed90b898af231974f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55aa8b20ceb3093657f5dbe3c905a5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c891df6a316f1c61e9ec",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6b53dc7cd9251ebc4cb3",
+    "url": "/static/js/2.14d5d1ff.chunk.js"
+  },
+  {
+    "revision": "c891df6a316f1c61e9ec",
+    "url": "/static/js/main.30caa959.chunk.js"
+  },
+  {
+    "revision": "91b16906825959e2e77e",
+    "url": "/static/js/runtime~main.cf10c6f3.js"
+  }
+]);
